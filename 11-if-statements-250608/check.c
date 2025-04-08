ba9031eba690d386ee459f942b64f51a42913536
Matthias Meissen <18502815+matthiasmeissen@users.noreply.github.com>
2025-04-08 17:28:15 +0200

    Add Project 11

---
+++ 11-if-statements-250608/check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int score = 100;
+
+    if (score == 100) {
+        printf("This is the correct score.\n");
+    }
+    if (score != 100) {
+        printf("This is not the correct score.\n");
+    }
+
+    return 0;
+}

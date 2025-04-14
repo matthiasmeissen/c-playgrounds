7f52f5e22c9da499c9e65a5dfa4442339acda146
Matthias Meissen <18502815+matthiasmeissen@users.noreply.github.com>
2025-04-14 13:03:03 +0200

    Finish For Loop

--- 17-for-loop-250414/loop.c
+++ 17-for-loop-250414/loop.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
 
+// A for loop has three steps
+// Initilization: int i = 0;
+// Condition: i <= 20;
+// Post Iteration: i++;
+// All these steps usually happen inside the loop head for()
+// Note that they can also be placed elsewhere
+// Initilization can happen outside: int i = 0; for(; i <= 20; i++) {}
+// Condition can happen inside: for(int i = 0; ; i++) {break;}
+// Post Iteration can happen inside: for(int i = 0; i <= 20; ) {i++}
+
+void printRange(int max, int step);
+int getInput(void);
+
 int main(void) {
-    for (int i = 0; i < 19; i += 2) {
-        printf("%d", i);
+    int max = getInput();
+    int step = getInput();
+    printRange(max, step);
+
+    // Examples
+    // Remove condition from for () part 
+    // Place break inside the loop
+
+    for (int i = 0; ; i += 1) {
+        printf("%d ", i);
+        if (i >= 20) {
+            printf("\n");
+            break;
+        }
     }
 
+    // Remove post iteration from for () part 
+    // Place inside the loop
+
+    for (int i = 0; i <= 20; ) {
+        printf("%d ", i);
+        i += 4;
+    }
+    printf("\n");
+
     return 0;
+}
+
+void printRange(int max, int step) {
+    for (int i = 0; i <= max; i += step) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
+int getInput(void) {
+    int input;
+    printf("Enter a number: ");
+    scanf("%d", &input);
+    return input;
 }
--- README.md
+++ README.md
@@ -94,9 +94,9 @@ A place to learn the C programming language.
 - ✅ **Day 16: `while` Loop**
     - **Topic:** Looping while a condition remains true. Pre-test loop. Need to manage loop variable inside the loop.
     - **Exercise:** Print numbers from 10 down to 1 using a `while` loop.
-*   **Day 17: `for` Loop**
-    *   **Topic:** Concise loop structure (initialization; condition; post-iteration step). Ideal for counter-controlled loops.
-    *   **Exercise:** Print the first 10 even numbers (0, 2, ..., 18) using a `for` loop.
+- ✅ **Day 17: `for` Loop**
+    - **Topic:** Concise loop structure (initialization; condition; post-iteration step). Ideal for counter-controlled loops.
+    - **Exercise:** Print the first 10 even numbers (0, 2, ..., 18) using a `for` loop.
 *   **Day 18: `do...while` Loop**
     *   **Topic:** Loop that executes the body at least once, then checks the condition. Post-test loop.
     *   **Exercise:** Create a simple menu prompt that asks the user for input and repeats *until* they enter 'q', using `do...while`. (Input handling will be basic for now).

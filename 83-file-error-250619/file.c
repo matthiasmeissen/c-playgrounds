a66c52998ec3f8fd7e7fe061745acf22e8b79253
matthiasmeissen <meissenmatthias@gmail.com>
2025-06-19 22:32:34 +0200

    Finish File Error

--- 83-file-error-250619/file.c
+++ 83-file-error-250619/file.c
@@ -15,10 +15,35 @@ int main(void) {
     }
     printf("Successfully opened the file.\n");
 
+    // Declare character array of fixed size to create a buffer for incoming characters in on line
     char lineBuffer[100] = {0};
+
+    // One way to read a single line from a file
     char * c = NULL;
-    c = fgets(lineBuffer, sizeof(lineBuffer), filePtr);
-    printf("%s\n", lineBuffer);
+    // Store return value of fgets in pointer 
+    // This is a pointer to lineBuffer when successfull, and NULL when failed
+    // c = fgets(lineBuffer, sizeof(lineBuffer), filePtr);
+    // printf("%s", lineBuffer);
+
+    // The ideomatic way to read a file line by line
+    while (fgets(lineBuffer, sizeof(lineBuffer), filePtr) != NULL) {
+        printf("%s", lineBuffer);
+    }
+
+    printf("\n --- Check file --- \n");
+
+    printf("feof() returns non-zero when FILE was correctly ended with EOF:     %d\n", feof(filePtr));
+    printf("ferror() returns 0, when there was no error:                        %d\n", ferror(filePtr));
+
+    // ferror() returns non-zero when true 
+    // feof() returns non-zero when true (Reached EOF)
+    if (ferror(filePtr)) {
+        fprintf(stderr, "Something went wrong while reading the file.\n");
+    } else if (feof(filePtr)) {
+        printf("Correctly reached the file with EOF.\n");
+    } else {
+        printf("Something else was with the file.\n");
+    }
 
     // Close filestream in if statement, check if not 0 for failure
     if (fclose(filePtr) != 0) {
